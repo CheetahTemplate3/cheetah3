ac8e5bdedc89c7ce1c29d283435ef27ceab54db4
tavis_rudd <tavis_rudd>
2001-06-18 19:26:30 +0000

    temporary fix for nested macro calls.  This will need a more permanent solution with proper parsing of macro arg lists.

--- src/CodeGenerator.py
+++ src/CodeGenerator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: CodeGenerator.py,v 1.4 2001/06/18 18:52:14 tavis_rudd Exp $
+# $Id: CodeGenerator.py,v 1.5 2001/06/18 19:26:30 tavis_rudd Exp $
 """Utilities, processors and filters for Cheetah's codeGenerator
 
 Cheetah's codeGenerator is designed to be extensible with plugin
@@ -10,12 +10,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.4 $
+Version: $Revision: 1.5 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/06/18 18:52:14 $
+Last Revision Date: $Date: 2001/06/18 19:26:30 $
 """
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.4 $"[11:-2]
+__version__ = "$Revision: 1.5 $"[11:-2]
 
 ##################################################
 ## DEPENDENCIES ##
@@ -404,8 +404,7 @@ def preProcessLazyMacroCalls(templateObj, templateDef):
             line = lineNumFromPos(match.string, match.start())
             raise Error('Undeclared variable $' + str(name) + \
                         ' used in macro call #'+ macroSignature + ' on line ' +
-                        str(line))
-        
+                        str(line))       
             
         validateMacroDirective(templateObj, macroArgstring)
         if macroName in templateObj._macros.keys():
@@ -527,8 +526,8 @@ def preProcessIncludeDirectives(templateObj, templateDef):
                 templateDef=includeString,
                 overwriteSettings=templateObj.settings(),
                 searchList=templateObj.searchList(),
-                cheetahBlocks=templateObj._cheetahBlocks)
-            nestedTemplate._macros = templateObj._macros
+                cheetahBlocks=templateObj._cheetahBlocks,
+                macros=templateObj._macros)
             templateObj._parsedIncludes[includeID] = nestedTemplate
             if not hasattr(nestedTemplate, 'respond'):
                 nestedTemplate.startServer()
