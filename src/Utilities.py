2f4cbcdbdbf2855174608d144281e174f4abca75
echuck <echuck>
2001-07-09 02:36:24 +0000

    make insertLines more robust

--- src/Utilities.py
+++ src/Utilities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Utilities.py,v 1.1 2001/06/13 03:50:39 tavis_rudd Exp $
+# $Id: Utilities.py,v 1.2 2001/07/09 02:36:24 echuck Exp $
 """Utility classes and functions used in the Cheetah package
 
 
@@ -8,12 +8,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.1 $
+Version: $Revision: 1.2 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/06/13 03:50:39 $
+Last Revision Date: $Date: 2001/07/09 02:36:24 $
 """
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.1 $"[11:-2]
+__version__ = "$Revision: 1.2 $"[11:-2]
 
 
 ##################################################
@@ -27,11 +27,12 @@ import types
 
 True = (1==1)
 False = (0==1)
-  
+
 ##################################################
 ## FUNCTIONS ##
 
 def insertLineNums(string):
+    string = str(string)
     lineNum = [0,]
     def lineNums(match, lineNum=lineNum):
         lineNum[0] +=1
@@ -57,7 +58,7 @@ def removeDuplicateValues(list):
         if not list[0] in listCopy:
             listCopy.append(list[0])
         del list[0]
-        
+
     return listCopy
 
 def mergeNestedDictionaries(dict1, dict2):
@@ -65,7 +66,7 @@ def mergeNestedDictionaries(dict1, dict2):
 
     This little function is very handy for selectively overriding settings in a
     settings dictionary that has a nested structure.  """
-    
+
     newDict = dict1.copy()
     for key,val in dict2.items():
         if newDict.has_key(key) and type(val) == types.DictType and type(newDict[key]) == types.DictType:
@@ -73,9 +74,9 @@ def mergeNestedDictionaries(dict1, dict2):
         else:
             newDict[key] = val
     return newDict
-    
 
-    
+
+
 ##################################################
 ## CLASSES ##
 
@@ -83,7 +84,7 @@ def mergeNestedDictionaries(dict1, dict2):
 
 ##################################################
 ## if run from the command line ##
-     
+
 if __name__ == '__main__':
     pass
 
