4c03795e9e64f56c62134d1120ab814c30f0b367
tavis_rudd <tavis_rudd>
2001-07-13 22:47:54 +0000

    updated the plugin so it works with the recent changes

--- src/Plugins/PSP.py
+++ src/Plugins/PSP.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: PSP.py,v 1.1 2001/06/13 03:50:40 tavis_rudd Exp $
+# $Id: PSP.py,v 1.2 2001/07/13 22:47:54 tavis_rudd Exp $
 """A plugin that allows Cheetah to handle PythonServerPages style coding
 
 Meta-Data
@@ -7,19 +7,19 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.1 $
+Version: $Revision: 1.2 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/06/13 03:50:40 $
+Last Revision Date: $Date: 2001/07/13 22:47:54 $
 """
 
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.1 $"[11:-2]
+__version__ = "$Revision: 1.2 $"[11:-2]
 
 ##################################################
 ## DEPENDENCIES ##
 
-from Cheetah.Delimeters import delimeters
-from Cheetah.Template import EXEC_TAG_TYPE
+from Cheetah.Delimiters import delimiters
+import Cheetah.CodeGenerator
 
 ##################################################
 ## CONSTANTS & GLOBALS ##
@@ -31,46 +31,51 @@ False = (0==1)
 ## FUNCTIONS ##
 
 
-def pythonScriptTagProcessor(templateObj, pythonScript):
-    """Process PSP style code that is embedded in the template definition."""
-
-    pythonScript = pythonScript.strip()
-    settings = templateObj._settings
-    indent = settings['codeGenerator']['indentationStep']
-    if not templateObj._codeGeneratorState.has_key('indentLevel'):
-        templateObj._codeGeneratorState['indentLevel'] = \
-                       settings['codeGenerator']['initialIndentLevel']
-
-    if pythonScript.lower() == 'end': # move out one indent level
-        templateObj._codeGeneratorState['indentLevel'] -= 1
-        outputCode = indent*templateObj._codeGeneratorState['indentLevel']
-
-    elif pythonScript.lower()[0:4] in ('else','elif') or \
-         pythonScript.lower().startswith('except'):
-        # continuation of previous block
-       
-        outputCode = indent*(templateObj._codeGeneratorState['indentLevel']-1) + \
-                     pythonScript +"\n" + \
-                     indent*templateObj._codeGeneratorState['indentLevel']
-
-    elif pythonScript[len(pythonScript.strip()) -1] == ":":
-        # it's the start of a new block
-        templateObj._codeGeneratorState['indentLevel'] += 1
-        outputCode = indent*(templateObj._codeGeneratorState['indentLevel']-1) + \
-                     pythonScript + "\n" + \
-                     indent*templateObj._codeGeneratorState['indentLevel']
-
-    elif pythonScript.lower().startswith('='):   # it's a python value eval()
-        outputCode = indent*(templateObj._codeGeneratorState['indentLevel']) + \
-               "outputList += [str(" + pythonScript[1:] + "),]\n" + \
-               indent*templateObj._codeGeneratorState['indentLevel']
-    else:                           # it's a chunk of plain python code              
-        outputCode = indent*(templateObj._codeGeneratorState['indentLevel']) + \
-                     pythonScript + "\n" + \
-                     indent*templateObj._codeGeneratorState['indentLevel']
-
-    return outputCode
-
+class pythonScriptTagProcessor(Cheetah.CodeGenerator.TagProcessor):
+    _token = 'pythonScript'
+    _tagType = Cheetah.CodeGenerator.EXEC_TAG_TYPE
+    _delimRegexs = [delimiters['<%,%>'], ]
+    
+    def translateTag(self, templateObj, pythonScript):
+        """Process PSP style code that is embedded in the template definition."""
+    
+        pythonScript = pythonScript.strip()
+        settings = templateObj._settings
+        indent = settings['indentationStep']
+        if not templateObj._codeGeneratorState.has_key('indentLevel'):
+            templateObj._codeGeneratorState['indentLevel'] = \
+                           settings['initialIndentLevel']
+    
+        if pythonScript.lower() == 'end': # move out one indent level
+            templateObj._codeGeneratorState['indentLevel'] -= 1
+            outputCode = indent*templateObj._codeGeneratorState['indentLevel']
+    
+        elif pythonScript.lower()[0:4] in ('else','elif') or \
+             pythonScript.lower().startswith('except'):
+            # continuation of previous block
+           
+            outputCode = indent*(templateObj._codeGeneratorState['indentLevel']-1) + \
+                         pythonScript +"\n" + \
+                         indent*templateObj._codeGeneratorState['indentLevel']
+    
+        elif pythonScript[len(pythonScript.strip()) -1] == ":":
+            # it's the start of a new block
+            templateObj._codeGeneratorState['indentLevel'] += 1
+            outputCode = indent*(templateObj._codeGeneratorState['indentLevel']-1) + \
+                         pythonScript + "\n" + \
+                         indent*templateObj._codeGeneratorState['indentLevel']
+    
+        elif pythonScript.lower().startswith('='):   # it's a python value eval()
+            outputCode = indent*(templateObj._codeGeneratorState['indentLevel']) + \
+                   "outputList += [str(" + pythonScript[1:] + "),]\n" + \
+                   indent*templateObj._codeGeneratorState['indentLevel']
+        else:                           # it's a chunk of plain python code              
+            outputCode = indent*(templateObj._codeGeneratorState['indentLevel']) + \
+                         pythonScript + "\n" + \
+                         indent*templateObj._codeGeneratorState['indentLevel']
+    
+        return outputCode
+    
 
 ##################################################
 ## CLASSES ##
@@ -81,12 +86,12 @@ class PSPplugin:
     
     def bindToTemplateServer(self, templateObj):
         """insert the settings neccessary for PSP into the templateObj"""
-        pythonScriptSettingsDict = {'type':EXEC_TAG_TYPE,
-                                    'processor':pythonScriptTagProcessor,
-                                    'delims':[delimeters['<%,%>'], ],
-                                    }
-        templateObj._settings['codeGenerator']['coreTags']['pythonScript'] = \
-                                                    pythonScriptSettingsDict
+        processor = pythonScriptTagProcessor()
+        
+        templateObj._settings['preProcessors'].append(
+            ('pythonScript', processor.preProcess) )
+
+        templateObj._settings['tagProcessors']['pythonScript'] = processor                                        
 
 
 ##################################################
