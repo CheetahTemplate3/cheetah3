e298b4817ce9e33757cfc698f58c098b3fbc93ea
tavis_rudd <tavis_rudd>
2001-08-03 21:48:34 +0000

    added support for #continue and #break in #for blocks.
    #for $i in range(15)
    #if $i == 10
    #continue
    $i
    #end for
    
    Note: #break and #continue will both move the indent level out one, so the
    "#end if" is not needed in the example above

--- src/CodeGenerator.py
+++ src/CodeGenerator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: CodeGenerator.py,v 1.16 2001/08/03 19:20:50 tavis_rudd Exp $
+# $Id: CodeGenerator.py,v 1.17 2001/08/03 21:48:34 tavis_rudd Exp $
 """Utilities, processors and filters for Cheetah's codeGenerator
 
 Cheetah's codeGenerator is designed to be extensible with plugin
@@ -10,12 +10,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.16 $
+Version: $Revision: 1.17 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/08/03 19:20:50 $
+Last Revision Date: $Date: 2001/08/03 21:48:34 $
 """
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.16 $"[11:-2]
+__version__ = "$Revision: 1.17 $"[11:-2]
 
 ##################################################
 ## DEPENDENCIES ##
@@ -75,7 +75,12 @@ class DisplayLogicProcessor(TagProcessor.TagProcessor):
         if tag in ('end if','end for'):
             templateObj._codeGeneratorState['indentLevel'] -= 1
             outputCode = indent*templateObj._codeGeneratorState['indentLevel']
-    
+
+        elif tag in ('continue','break'):
+            outputCode = indent*templateObj._codeGeneratorState['indentLevel'] + tag + "\n"
+            templateObj._codeGeneratorState['indentLevel'] -= 1
+            outputCode += indent*templateObj._codeGeneratorState['indentLevel']
+
         elif tag[0:4] in ('else','elif'):
             tag = tag.replace('else if','elif')
             
--- src/Delimiters.py
+++ src/Delimiters.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Delimiters.py,v 1.3 2001/07/13 22:48:14 tavis_rudd Exp $
+# $Id: Delimiters.py,v 1.4 2001/08/03 21:48:34 tavis_rudd Exp $
 """A dictionary of delimeter regular expressions that are used in Cheetah
 
 Meta-Data
@@ -7,12 +7,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.3 $
+Version: $Revision: 1.4 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/07/13 22:48:14 $
+Last Revision Date: $Date: 2001/08/03 21:48:34 $
 """
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.3 $"[11:-2]
+__version__ = "$Revision: 1.4 $"[11:-2]
 
 
 ##################################################
@@ -56,6 +56,8 @@ delimiters = {
                                         r'else[\t ]if[\t ]+[^(?:/#)]+?|' +
                                         r'elif[\t ]+[^(?:/#)]+?|' +
                                         r'for[\t ][^(?:/#)]+?|' +
+                                        r'continue|' +
+                                        r'break|' +
                                         r'end if|' +
                                         r'end for|' +
                                         r')[\t ]*(?:\r\n|\n|\r|\Z)',
