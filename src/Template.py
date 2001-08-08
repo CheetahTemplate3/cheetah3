06b60adc9b0bf0cf8d8da7c4ddf7692dee3c234a
tavis_rudd <tavis_rudd>
2001-08-08 05:35:12 +0000

    undid the removal of the RESTART token

--- src/Template.py
+++ src/Template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Template.py,v 1.21 2001/08/08 02:35:31 tavis_rudd Exp $
+# $Id: Template.py,v 1.22 2001/08/08 05:35:12 tavis_rudd Exp $
 """Provides the core Template class for Cheetah
 See the docstring in __init__.py and the User's Guide for more information
 
@@ -8,12 +8,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.21 $
+Version: $Revision: 1.22 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/08/08 02:35:31 $
+Last Revision Date: $Date: 2001/08/08 05:35:12 $
 """ 
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.21 $"[11:-2]
+__version__ = "$Revision: 1.22 $"[11:-2]
 
 
 ##################################################
@@ -57,6 +57,13 @@ False = (0==1)
 class Error(Exception):
     pass
 
+class RESTART:
+    """A token class that the #include directive can use to signal that the
+    codeGenerator needs to restart and parse new stuff that has been included"""
+    
+    def __init__(self, templateDef):
+        self.data = templateDef
+    
 class Template(SettingsManager):
     """The core template engine: parses, compiles, and serves templates."""
 
@@ -361,6 +368,9 @@ class Template(SettingsManager):
             if debug: results['stage1'] = []
             for preProcessor in settings['preProcessors']:
                 templateDef = preProcessor[1](self, templateDef)
+                if isinstance(templateDef, RESTART):
+                    # a parser restart might have been requested for #include's 
+                    return self._codeGenerator(templateDef.data)
                 if debug: results['stage1'].append((preProcessor[0], templateDef))
 
             ## stage 2 - generate the python code for each of the tokenized tags ##
