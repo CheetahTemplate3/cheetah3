b77431f6c127d797e49203d88df64fb752e96d58
tavis_rudd <tavis_rudd>
2001-08-10 03:36:38 +0000

    added Tim Peter's unique() from the Python Cookbook

--- src/Utilities.py
+++ src/Utilities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Utilities.py,v 1.3 2001/08/08 06:31:07 tavis_rudd Exp $
+# $Id: Utilities.py,v 1.4 2001/08/10 03:36:38 tavis_rudd Exp $
 """Utility classes and functions used in the Cheetah package
 
 
@@ -8,12 +8,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.3 $
+Version: $Revision: 1.4 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/08/08 06:31:07 $
+Last Revision Date: $Date: 2001/08/10 03:36:38 $
 """
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.3 $"[11:-2]
+__version__ = "$Revision: 1.4 $"[11:-2]
 
 
 ##################################################
@@ -58,15 +58,75 @@ def lineNumFromPos(string, pos):
     return len(string[0:pos].split('\n'))
 
 
-def removeDuplicateValues(list):
-    """Remove all duplicate values in a list."""
-    listCopy = []
-    while len(list) > 0:
-        if not list[0] in listCopy:
-            listCopy.append(list[0])
-        del list[0]
+def unique(s):
+    """Return a list of the elements in s, but without duplicates.
 
-    return listCopy
+    For example, unique([1,2,3,1,2,3]) is some permutation of [1,2,3],
+    unique("abcabc") some permutation of ["a", "b", "c"], and
+    unique(([1, 2], [2, 3], [1, 2])) some permutation of
+    [[2, 3], [1, 2]].
+
+    For best speed, all sequence elements should be hashable.  Then
+    unique() will usually work in linear time.
+
+    If not possible, the sequence elements should enjoy a total
+    ordering, and if list(s).sort() doesn't raise TypeError it's
+    assumed that they do enjoy a total ordering.  Then unique() will
+    usually work in O(N*log2(N)) time.
+
+    If that's not possible either, the sequence elements must support
+    equality-testing.  Then unique() will usually work in quadratic
+    time.
+
+    This method was copied from Tim Peter's contribution to The Python Cookbook.
+    """
+
+    n = len(s)
+    if n == 0:
+        return []
+
+    # Try using a dict first, as that's the fastest and will usually
+    # work.  If it doesn't work, it will usually fail quickly, so it
+    # usually doesn't cost much to *try* it.  It requires that all the
+    # sequence elements be hashable, and support equality comparison.
+    u = {}
+    try:
+        for x in s:
+            u[x] = 1
+    except TypeError:
+        del u  # move on to the next method
+    else:
+        return u.keys()
+
+    # We can't hash all the elements.  Second fastest is to sort,
+    # which brings the equal elements together; then duplicates are
+    # easy to weed out in a single pass.
+    # NOTE:  Python's list.sort() was designed to be efficient in the
+    # presence of many duplicate elements.  This isn't true of all
+    # sort functions in all languages or libraries, so this approach
+    # is more effective in Python than it may be elsewhere.
+    try:
+        t = list(s)
+        t.sort()
+    except TypeError:
+        del t  # move on to the next method
+    else:
+        assert n > 0
+        last = t[0]
+        lasti = i = 1
+        while i < n:
+            if t[i] != last:
+                t[lasti] = last = t[i]
+                lasti += 1
+            i += 1
+        return t[:lasti]
+
+    # Brute force is all that's left.
+    u = []
+    for x in s:
+        if x not in u:
+            u.append(x)
+    return u
 
 def mergeNestedDictionaries(dict1, dict2):
     """Recursively merge the values of dict2 into dict1.
