0587a99ec7d9dc31bc6484ed61d82ed41a238a91
tavis_rudd <tavis_rudd>
2001-08-12 19:09:10 +0000

    finished implementing C versions of valueForKey and valueForName - needs checking for memory leaks!!!

--- src/_namemapper.c
+++ src/_namemapper.c
@@ -10,78 +10,121 @@ static PyObject *ErrorObject;   /* locally-raised exception */
 #define onError(message) \
     { PyErr_SetString(ErrorObject, message); return NULL; }
 
-/* *************************************************************************** */
-/* Exported module method-functions */
+#define MAXCHUNKS 50		/* max num of nameChunks for the arrays */
 
+/* *************************************************************************** */
+/* First the c versions of the functions */
+/* *************************************************************************** */
+
+static PyObject *
+PyNamemapper_valueForKey(PyObject *obj, char *key)
+{
+  PyObject *theValue;
+  const char *underscore = "_";
+  char *underscoreKey;
+
+  if (PyObject_HasAttrString(obj, key)) {
+    theValue = PyObject_GetAttrString(obj, key);
+  } else if (PyMapping_Check(obj) && PyMapping_HasKeyString(obj, key)) {
+    theValue =  PyMapping_GetItemString(obj, key);
+  } else {
+    underscoreKey = malloc(strlen(key) + strlen(underscore) + 1);
+    strcpy(underscoreKey, underscore);
+    strcat(underscoreKey, key);
+
+    if (PyObject_HasAttrString(obj, underscoreKey)) {
+      theValue = PyObject_GetAttrString(obj, underscoreKey);
+    } else {
+      onError(key);
+    }
+    free(underscoreKey);
+  }
+  /* @@ Do I need to: Py_INCREF(theValue); */
+  return theValue;		
+}
+
+static PyObject *
+PyNamemapper_valueForName(PyObject *obj, char *nameChunks[], 
+			  int numChunks, 
+			  int executeCallables)
+{
+  char *firstKey;
+  PyObject *binding;
+  char *remainingChunks[MAXCHUNKS]; 
+  int i;
+
+  firstKey = nameChunks[0];
+
+  if (! (binding = PyNamemapper_valueForKey(obj, firstKey))){
+    return NULL;
+  }
+  
+  if (executeCallables && PyCallable_Check(binding) && (!PyInstance_Check(binding)) 
+      && (!PyClass_Check(binding)) ) {
+    binding = PyObject_CallObject(binding, NULL);
+  }
+  
+  if (numChunks > 1) {
+    for (i=1; i < numChunks; i++) {
+      remainingChunks[i-1] = nameChunks[i];
+    }
+    if (!(binding = PyNamemapper_valueForName(binding, remainingChunks, 
+					      numChunks - 1, executeCallables))) {
+      return NULL;
+    }
+    return binding;
+  } else {
+    return binding;
+  }
+}
+
+/* *************************************************************************** */
+/* Now the module functions to export into Python */
+/* *************************************************************************** */
 
 static PyObject *
 namemapper_valueForKey(PyObject *self, PyObject *args)       /* args: (string) */
 {
   PyObject *obj;
-  PyObject *theValue;
   char *key;
-  static char *underscore = "_";
-  char *underscoreKey;
 
   if (!PyArg_ParseTuple(args, "Os", &obj, &key)) {
     return NULL;
   }
 
-  if (PyObject_HasAttrString(obj, key)) {
-    return PyObject_GetAttrString(obj, key);
-  } else if (PyMapping_Check(obj) && PyMapping_HasKeyString(obj, key)) {
-    return PyMapping_GetItemString(obj, key);
-  }
-
-  underscoreKey = malloc(strlen(key) + strlen(underscore) + 1);
-  strcpy(underscoreKey, underscore);
-  strcat(underscoreKey, key);
-
-  if (PyObject_HasAttrString(obj, underscoreKey)) {
-    theValue = PyObject_GetAttrString(obj, underscoreKey);
-  } else {
-    theValue = Py_BuildValue(""); //actually I just want to raise an error
-  }
-  free(underscoreKey);
-  return theValue;
+  return PyNamemapper_valueForKey(obj, key);
 }
 
 static PyObject *
-namemapper_valueForKey2(PyObject *self, PyObject *args, PyObject *keywds)       /* args: (string) */
+namemapper_valueForName(PyObject *self, PyObject *args, PyObject *keywds)       /* args: (string) */
 {
 
   PyObject *obj;
-  PyObject *theValue;
-  PyObject *theDefault;
-  char *key;
-  static char *underscore = "_";
-  char *underscoreKey;
+  const char *name;
+  int executeCallables = 0;
 
-  static char *kwlist[] = {"obj", "key", "default", NULL};
+  char *dot = ".";
+  char *copyOfName;
+  char *nameChunks[MAXCHUNKS];
+  int numChunks = 1;
+  char *nextChunk;
 
-  if (!PyArg_ParseTupleAndKeywords(args, keywds, "Os|O", kwlist,  &obj, &key, &theDefault)) {
+  static char *kwlist[] = {"obj", "name", "executeCallables", NULL};
+
+  if (!PyArg_ParseTupleAndKeywords(args, keywds, "Os|i", kwlist,  &obj, &name, &executeCallables)) {
     return NULL;
   }
 
-  if (PyObject_HasAttrString(obj, key)) {
-    return PyObject_GetAttrString(obj, key);
-  } else if (PyMapping_Check(obj) && PyMapping_HasKeyString(obj, key)) {
-    return PyMapping_GetItemString(obj, key);
-  }
+  copyOfName = malloc(strlen(name) + 1);
+  copyOfName = strcpy(copyOfName, name);
 
-  theDefault = Py_BuildValue("s", "NoDefaultGiven");
-  underscoreKey = malloc(strlen(key) + strlen(underscore) + 1);
-  strcpy(underscoreKey, underscore);
-  strcat(underscoreKey, key);
-
-  if (PyObject_HasAttrString(obj, underscoreKey)) {
-    theValue = PyObject_GetAttrString(obj, underscoreKey);
-  } else {
-    theValue = theDefault;
+  nameChunks[0] = strtok(copyOfName, dot);
+  while ((nextChunk = strtok(NULL, dot)) != NULL) {
+    nameChunks[numChunks++] = nextChunk;
   }
-  free(underscoreKey);
-  return theValue;
-  
+  /* @@ DO I NEED TO: free(copyOfName); */
+
+  return PyNamemapper_valueForName(obj, nameChunks, numChunks, executeCallables);
 }
 
 /* *************************************************************************** */
@@ -89,7 +132,7 @@ namemapper_valueForKey2(PyObject *self, PyObject *args, PyObject *keywds)       
 
 static struct PyMethodDef namemapper_methods[] = {
   {"valueForKey", namemapper_valueForKey,  1},
-  {"valueForKey2", (PyCFunction)namemapper_valueForKey2,  METH_VARARGS|METH_KEYWORDS},
+  {"valueForName", (PyCFunction)namemapper_valueForName,  METH_VARARGS|METH_KEYWORDS},
   {NULL,         NULL}
 };
 
@@ -107,7 +150,7 @@ void init_namemapper()
   /* add symbolic constants to the module */
   d = PyModule_GetDict(m);
   ErrorObject = Py_BuildValue("s", "namemapper.error");   /* export exception */
-  PyDict_SetItemString(d, "error", ErrorObject);       /* add more if need */
+  PyDict_SetItemString(d, "NotFound", ErrorObject);       /* add more if need */
   
   /* check for errors */
   if (PyErr_Occurred())
