a2aac765574334505e109c27ca53662e97f36c0a
tavis_rudd <tavis_rudd>
2001-08-16 22:20:37 +0000

    added a comment

--- src/Template.py
+++ src/Template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Template.py,v 1.43 2001/08/16 22:15:18 tavis_rudd Exp $
+# $Id: Template.py,v 1.44 2001/08/16 22:20:37 tavis_rudd Exp $
 """Provides the core Template class for Cheetah
 See the docstring in __init__.py and the User's Guide for more information
 
@@ -8,12 +8,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.43 $
+Version: $Revision: 1.44 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/08/16 22:15:18 $
+Last Revision Date: $Date: 2001/08/16 22:20:37 $
 """ 
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.43 $"[11:-2]
+__version__ = "$Revision: 1.44 $"[11:-2]
 
 
 ##################################################
@@ -364,11 +364,11 @@ class Template(SettingsManager, Parser):
                               ('includeDirective',
                                includeDirective),
 
-                              ('macroCall',
+                              ('lazyMacroCall',
                                lazyMacroCall),
                               ('CallMacro',
                                callMacroDirective),
-                              ('macroCall', # get rid of this dbl-call
+                              ('lazyMacroCall', # check again after #callMacro directive
                                lazyMacroCall),
                              
                               ('rawDirective',
