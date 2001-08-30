ddd8b63746f7a5a690e27c02ae69250d450ccfc7
tavis_rudd <tavis_rudd>
2001-08-30 19:36:21 +0000

    initial import. This will eventually be used when streaming behaviour is introduced to Cheetah.

---
+++ src/DummyTransaction.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# $Id: DummyTransaction.py,v 1.1 2001/08/30 19:36:21 tavis_rudd Exp $
+
+"""Provides dummy Transaction and Response classes is used by Cheetah in place
+of real Webware transactions when the Template obj is not used directly as a
+Webware servlet.
+
+Meta-Data
+==========
+Author: Tavis Rudd <tavis@calrudd.com>
+Version: $Revision: 1.1 $
+Start Date: 2001/08/30
+Last Revision Date: $Date: 2001/08/30 19:36:21 $
+"""
+__author__ = "Tavis Rudd <tavis@calrudd.com>"
+__version__ = "$Revision: 1.1 $"[11:-2]
+
+##################################################
+## DEPENDENCIES ##
+
+try:
+    from StringIO import StringIO
+except:
+    from cStringIO import StringIO
+
+##################################################
+## CONSTANTS & GLOBALS ##
+
+True = (1==1)
+False = (1==0)
+
+##################################################
+## CLASSES ##
+
+class DummyResponse:
+    
+    """A dummy Response class is used by Cheetah in place of real Webware
+    Response objects when the Template obj is not used directly as a Webware
+    servlet.  """
+
+    
+    def __init__(self):
+        self._stringIO = StringIO()
+        self.write = self._stringIO.write
+        self.flush = self._stringIO.flush
+        self.getvalue = self._stringIO.getvalue()
+
+        # this might be used for output buffering at some stage in Cheetah
+        # but isn't yet.
+        self.writelines = self._stringIO.writelines
+        
+    def writeln(self, txt):
+        self.write(txt + '\n')
+
+class DummyTransaction:
+
+    """A dummy Transaction class is used by Cheetah in place of real Webware
+    transactions when the Template obj is not used directly as a Webware
+    servlet.
+
+    It only provides a response object and method.  All other methods and
+    attributes make no sense in this context.  """
+    
+    def __init__(self):
+        self._response = DummyResponse()
+
+    def reponse(self):
+        """Return a ref to the dummy reponse object."""
+       self._response
+       
