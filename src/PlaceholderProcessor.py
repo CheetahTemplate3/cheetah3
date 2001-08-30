0661c4357a3b4e3ab66960299560cfa06c9cf171
tavis_rudd <tavis_rudd>
2001-08-30 17:03:09 +0000

    fixed the error with errorChecker that Mike found.

--- src/PlaceholderProcessor.py
+++ src/PlaceholderProcessor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: PlaceholderProcessor.py,v 1.30 2001/08/16 22:15:18 tavis_rudd Exp $
+# $Id: PlaceholderProcessor.py,v 1.31 2001/08/30 17:03:09 tavis_rudd Exp $
 """Provides utilities for processing $placeholders in Cheetah templates
 
 Meta-Data
@@ -7,12 +7,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>,
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.30 $
+Version: $Revision: 1.31 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/08/16 22:15:18 $
+Last Revision Date: $Date: 2001/08/30 17:03:09 $
 """
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.30 $"[11:-2]
+__version__ = "$Revision: 1.31 $"[11:-2]
 
 
 ##################################################
@@ -103,6 +103,9 @@ class PlaceholderProcessor(TagProcessor):
             methodStr = ''
             formatterAutoArgs = ''
 
+        formatterTag = 'theFormatters["' + currFormatterID + '"]' + methodStr + '(' \
+                        + translatedTag + formatterAutoArgs + ')'
+
         ## use the errorChecker if enabled
         if self._errorChecker:
             originalTag = tag.replace(self.setting('placeholderMarker'),
@@ -119,11 +122,7 @@ class PlaceholderProcessor(TagProcessor):
             self._errorChecker.set(ID, originalTag, formatterTag)
             translatedTag = 'errorChecker.get("' + ID + '", trans=trans,localsDict=locals())'
             
-        
         ## deal with caching
-        formatterTag = 'theFormatters["' + currFormatterID + '"]' + methodStr + '(' \
-                        + translatedTag + formatterAutoArgs + ')'
-
         if cacheType == STATIC_CACHE:
             return self.evalPlaceholderString(formatterTag)
         elif cacheType == TIMED_REFRESH_CACHE:
