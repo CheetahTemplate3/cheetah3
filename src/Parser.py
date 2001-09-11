96892738fd9991e70e2ee67ef129cbeee18190ce
tavis_rudd <tavis_rudd>
2001-09-11 03:44:13 +0000

    fixed a bug in local var handling that Chuck found

--- src/Parser.py
+++ src/Parser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Parser.py,v 1.13 2001/08/16 22:15:18 tavis_rudd Exp $
+# $Id: Parser.py,v 1.14 2001/09/11 03:44:13 tavis_rudd Exp $
 """Parser base-class for Cheetah's TagProcessor class and for the Template class
 
 Meta-Data
@@ -7,12 +7,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.13 $
+Version: $Revision: 1.14 $
 Start Date: 2001/08/01
-Last Revision Date: $Date: 2001/08/16 22:15:18 $
+Last Revision Date: $Date: 2001/09/11 03:44:13 $
 """
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.13 $"[11:-2]
+__version__ = "$Revision: 1.14 $"[11:-2]
 
 ##################################################
 ## DEPENDENCIES ##
@@ -356,6 +356,7 @@ class Parser:
                 nameMapperPartOfName, remainderOfName = \
                                       name[0:firstSpecialChar], name[firstSpecialChar:]
                 remainderOfName = remainderOfName
+                nameChunks = nameMapperPartOfName.split('.')
             else:
                 nameMapperPartOfName = name
                 remainderOfName = ''
