6418e5bd4fcbae40045e3c52b3acfee47fca2547
tavis_rudd <tavis_rudd>
2001-09-15 23:57:48 +0000

    got rid of some junk

--- src/StopDirective.py
+++ src/StopDirective.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: StopDirective.py,v 1.3 2001/08/30 20:37:57 tavis_rudd Exp $
+# $Id: StopDirective.py,v 1.4 2001/09/15 23:57:48 tavis_rudd Exp $
 """StopDirective Processor class Cheetah's codeGenerator
 
 Meta-Data
@@ -7,29 +7,21 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.3 $
+Version: $Revision: 1.4 $
 Start Date: 2001/08/01
-Last Revision Date: $Date: 2001/08/30 20:37:57 $
+Last Revision Date: $Date: 2001/09/15 23:57:48 $
 """
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.3 $"[11:-2]
+__version__ = "$Revision: 1.4 $"[11:-2]
 
 ##################################################
 ## DEPENDENCIES ##
 
 # intra-package imports ...
 import TagProcessor
-##################################################
-## CONSTANTS & GLOBALS ##
-
-True = (1==1)
-False = (0==1)
 
 ##################################################
 ## CLASSES ##
-
-class Error(Exception):
-    pass
 
 class StopDirective(TagProcessor.TagProcessor):
     _tagType = TagProcessor.EXEC_TAG_TYPE
