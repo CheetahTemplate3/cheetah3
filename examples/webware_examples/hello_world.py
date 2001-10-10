61d804b4831a063a4deacdd5a55453c9e3eb5da7
tavis_rudd <tavis_rudd>
2001-10-10 22:05:01 +0000

    *** empty log message ***

--- examples/webware_examples/hello_world.py
+++
@@ -1,8 +0,0 @@
-templateDef = '''
-Hello World!
-'''
-
-from Cheetah.Templates.SkeletonPage import SkeletonPage
-class hello_world(SkeletonPage):
-    def initializeTemplate(self):
-        self.extendTemplate(templateDef)
