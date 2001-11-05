a8f5550870e901886f5d9ea761844872188795b4
tavis_rudd <tavis_rudd>
2001-11-05 06:51:48 +0000

    added the new CheetahCompile test module

--- src/Tests/Test.py
+++ src/Tests/Test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Test.py,v 1.27 2001/10/10 06:57:22 tavis_rudd Exp $
+# $Id: Test.py,v 1.28 2001/11/05 06:51:48 tavis_rudd Exp $
 """Unit-testing framework for the Cheetah package
 
 TODO
@@ -16,12 +16,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>,
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.27 $
+Version: $Revision: 1.28 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/10/10 06:57:22 $
+Last Revision Date: $Date: 2001/11/05 06:51:48 $
 """
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.27 $"[11:-2]
+__version__ = "$Revision: 1.28 $"[11:-2]
 
 
 ##################################################
@@ -39,10 +39,16 @@ False = (0==1)
 ##################################################
 ## if run from the command line
 if __name__ == '__main__':
-    import SyntaxAndOutput, NameMapper, Template
-    
+    import SyntaxAndOutput
     suite = unittest.findTestCases(SyntaxAndOutput)
+    
+    import NameMapper
     suite.addTest( unittest.findTestCases(NameMapper) )
+    
+    import Template
     suite.addTest( unittest.findTestCases(Template) )
+    
+    import CheetahCompile
+    suite.addTest( unittest.findTestCases(CheetahCompile) )
     
     unittest.TextTestRunner().run(suite)
