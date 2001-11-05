f593c827af444aae9639b18843281d2d47d38502
tavis_rudd <tavis_rudd>
2001-11-05 06:53:18 +0000

    update

--- src/Tests/Test.py
+++ src/Tests/Test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# $Id: Test.py,v 1.28 2001/11/05 06:51:48 tavis_rudd Exp $
-"""Unit-testing framework for the Cheetah package
+# $Id: Test.py,v 1.29 2001/11/05 06:53:18 tavis_rudd Exp $
+"""Core module of Cheetah's Unit-testing framework
 
 TODO
 ================================================================================
@@ -16,12 +16,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>,
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.28 $
+Version: $Revision: 1.29 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/11/05 06:51:48 $
+Last Revision Date: $Date: 2001/11/05 06:53:18 $
 """
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.28 $"[11:-2]
+__version__ = "$Revision: 1.29 $"[11:-2]
 
 
 ##################################################
