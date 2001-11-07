9225a462eb2c714684600caeedbe2aeabc9c796d
hierro <hierro>
2001-11-07 02:44:28 +0000

    Changed constructor arg 'outputFilter' to 'filter' to parallel the #filter directive.

--- src/Template.py
+++ src/Template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Template.py,v 1.63 2001/11/06 03:50:11 tavis_rudd Exp $
+# $Id: Template.py,v 1.64 2001/11/07 02:44:28 hierro Exp $
 """Provides the core Template class for Cheetah
 See the docstring in __init__.py and the User's Guide for more information
 
@@ -8,12 +8,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.63 $
+Version: $Revision: 1.64 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/11/06 03:50:11 $
+Last Revision Date: $Date: 2001/11/07 02:44:28 $
 """ 
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__version__ = "$Revision: 1.63 $"[11:-2]
+__version__ = "$Revision: 1.64 $"[11:-2]
 
 ##################################################
 ## DEPENDENCIES
@@ -64,7 +64,7 @@ class Template(SettingsManager, Servlet):
 
     def __init__(self, source=None, searchList=[], file=None,
                  settings={},           # user settings that are visible in templates
-                 outputFilter='ReplaceNone', # which filter from Cheetah.Filters
+                 filter='ReplaceNone', # which filter from Cheetah.Filters
                  filtersLib=Filters,
                  errorCatcher=None,
                  
@@ -105,6 +105,8 @@ class Template(SettingsManager, Servlet):
                 
         ##################################################
         ## setup the ouput filters
+        outputFilter = filter # Kludge: this existing stanza assumes the
+                # keyword argument is 'outputFilter' and uses local var 'filter'                # for a conflicting purpose.
         self._filtersLib = filtersLib
         self._filters = {}
         if outputFilter:
