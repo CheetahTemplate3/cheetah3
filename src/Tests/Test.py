28fc8593518304a40ed67e88d9aa30deacc9c005
tavis_rudd <tavis_rudd>
2002-03-07 03:54:58 +0000

    refactoring

--- src/Tests/Test.py
+++ src/Tests/Test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Test.py,v 1.31 2001/12/19 02:10:25 tavis_rudd Exp $
+# $Id: Test.py,v 1.32 2002/03/07 03:54:58 tavis_rudd Exp $
 """Core module of Cheetah's Unit-testing framework
 
 TODO
@@ -16,12 +16,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>,
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.31 $
+Version: $Revision: 1.32 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2001/12/19 02:10:25 $
+Last Revision Date: $Date: 2002/03/07 03:54:58 $
 """
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__revision__ = "$Revision: 1.31 $"[11:-2]
+__revision__ = "$Revision: 1.32 $"[11:-2]
 
 
 ##################################################
@@ -37,21 +37,27 @@ True = (1==1)
 False = (0==1)
 
 ##################################################
+## CLASSES
+class RunTests:
+    def __init__(self):
+        import SyntaxAndOutput
+        suite = unittest.findTestCases(SyntaxAndOutput)
+        
+        import NameMapper
+        suite.addTest( unittest.findTestCases(NameMapper) )
+        
+        import Template
+        suite.addTest( unittest.findTestCases(Template) )
+        
+        import CheetahCompile
+        suite.addTest( unittest.findTestCases(CheetahCompile) )
+    
+        import FileRefresh
+        suite.addTest( unittest.findTestCases(FileRefresh) )
+        
+        unittest.TextTestRunner().run(suite)
+
+##################################################
 ## if run from the command line
 if __name__ == '__main__':
-    import SyntaxAndOutput
-    suite = unittest.findTestCases(SyntaxAndOutput)
-    
-    import NameMapper
-    suite.addTest( unittest.findTestCases(NameMapper) )
-    
-    import Template
-    suite.addTest( unittest.findTestCases(Template) )
-    
-    import CheetahCompile
-    suite.addTest( unittest.findTestCases(CheetahCompile) )
-
-    import FileRefresh
-    suite.addTest( unittest.findTestCases(FileRefresh) )
-    
-    unittest.TextTestRunner().run(suite)
+    RunTests()
