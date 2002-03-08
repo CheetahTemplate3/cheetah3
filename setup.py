90e0faebc8f6ed5c032c20795fcab4a10143fc0b
tavis_rudd <tavis_rudd>
2002-03-08 17:01:00 +0000

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
-# $Id: setup.py,v 1.12 2002/03/06 18:05:50 tavis_rudd Exp $
+# $Id: setup.py,v 1.13 2002/03/08 17:01:00 tavis_rudd Exp $
 
 try:                           # see if WebwareExp's Setup procedure can be used
     from src._properties import Version, Description
-    long_description = open('README').read()
     settings= {'name': 'Cheetah',
                'version':Version,
-               'long_description':long_description,
                'description':Description,
                }
     from Webware.Setup import SetupManager
     SetupManager._rebuildManifestTemplate = 0
     SetupManager(searchPath=['.'],settings=settings)
-except ImportError:
+except:
     import SetupTools
     import SetupConfig
     configurations = (SetupConfig,)
