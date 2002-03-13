36e5adba31f2b6d5ad04813a5fca0d8f8d4d8697
tavis_rudd <tavis_rudd>
2002-03-13 18:39:07 +0000

    fixed up the reference to self.session

--- src/Servlet.py
+++ src/Servlet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Servlet.py,v 1.16 2002/03/12 07:49:19 tavis_rudd Exp $
+# $Id: Servlet.py,v 1.17 2002/03/13 18:39:07 tavis_rudd Exp $
 """Provides an abstract Servlet baseclass for Cheetah's Template class
 
 Meta-Data
@@ -7,12 +7,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.16 $
+Version: $Revision: 1.17 $
 Start Date: 2001/10/03
-Last Revision Date: $Date: 2002/03/12 07:49:19 $
+Last Revision Date: $Date: 2002/03/13 18:39:07 $
 """ 
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__revision__ = "$Revision: 1.16 $"[11:-2]
+__revision__ = "$Revision: 1.17 $"[11:-2]
 
 ##################################################
 ## CONSTANTS & GLOBALS
@@ -43,6 +43,9 @@ except:
         def sleep(self, transaction):
             pass
 
+        def shutdown(self):
+            pass
+
 ##################################################
 ## CLASSES
 
@@ -62,12 +65,13 @@ class Servlet(BaseServlet):
     ## methods called by Webware during the request-response
         
     def awake(self, transaction):
+        BaseServlet.awake(self, transaction)        
         self.transaction = transaction        
         self.application = transaction.application
         self.response = response = transaction.response
         self.request = transaction.request
         self.session = transaction.session
-        self.write = response.write
+        self.write = response().write
         #self.writeln = response.writeln
         
     def respond(self, trans=None):
@@ -76,15 +80,11 @@ class Servlet(BaseServlet):
     __str__ = respond
 
     def sleep(self, transaction):
-        self._session = None
+        BaseServlet.sleep(self, transaction)
+        self.session = None
         self.request  = None
         self.response = None
         self.transaction = None
-
-    def session(self):
-        if not self._session:
-            self._session = self._transaction.session()
-        return self._session
 
     def shutdown(self):
         pass
