877db61b807f3232bec8a9c53825026d98fe3d14
hierro <hierro>
2002-03-17 21:03:39 +0000

    Fix missing """.

--- src/Utils/Misc.py
+++ src/Utils/Misc.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
-# $Id: Misc.py,v 1.1 2002/03/16 19:29:13 hierro Exp $
+# $Id: Misc.py,v 1.2 2002/03/17 21:03:39 hierro Exp $
 """Miscellaneous functions/objects used by Cheetah but also useful standalone.
-"""
 
 Meta-Data
 ================================================================================
 Author: Mike Orr <iron@mso.oz.net>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.1 $
+Version: $Revision: 1.2 $
 Start Date: 2001/11/07
-Last Revision Date: $Date: 2002/03/16 19:29:13 $
+Last Revision Date: $Date: 2002/03/17 21:03:39 $
 """ 
 __author__ = "Mike Orr <iron@mso.oz.net>"
-__revision__ = "$Revision: 1.1 $"[11:-2]
+__revision__ = "$Revision: 1.2 $"[11:-2]
 
 ##################################################
 ## DEPENDENCIES
