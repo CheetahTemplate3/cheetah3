2cc296badff320b0e6f5f165b635b24dc72ec40f
tavis_rudd <tavis_rudd>
2002-03-17 06:43:31 +0000

    fixed searchList sharing bug with #include'd templates, as reported by Johannes

--- src/Template.py
+++ src/Template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Template.py,v 1.84 2002/02/25 21:20:29 tavis_rudd Exp $
+# $Id: Template.py,v 1.85 2002/03/17 06:43:31 tavis_rudd Exp $
 """Provides the core Template class for Cheetah
 See the docstring in __init__.py and the User's Guide for more information
 
@@ -8,12 +8,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.84 $
+Version: $Revision: 1.85 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2002/02/25 21:20:29 $
+Last Revision Date: $Date: 2002/03/17 06:43:31 $
 """ 
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__revision__ = "$Revision: 1.84 $"[11:-2]
+__revision__ = "$Revision: 1.85 $"[11:-2]
 
 ##################################################
 ## DEPENDENCIES
@@ -144,6 +144,7 @@ class Template(SettingsManager, Servlet):
         if KWs.has_key('_preBuiltSearchList'):
             # happens with nested Template obj creation from #include's
             self._searchList = list(KWs['_preBuiltSearchList'])
+            self._searchList.append(self)
         else:
             # create our own searchList
             self._searchList = [self._globalSetVars]            
