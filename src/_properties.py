9fb0a014959a292e1f074214957f63c8c1be3178
tavis_rudd <tavis_rudd>
2002-03-26 19:16:54 +0000

    update

--- src/_properties.py
+++ src/_properties.py
@@ -1,7 +1,7 @@
 PackageName = "Cheetah"
 Title = "Cheetah: The Python-Powered Template Engine"
 Description = "Provides Cheetah, a Python-powered template engine and code generator"
-Status = 'Production'
+Status = 'Beta'
 Version = '0.9.12b2'
 Maintainer = "Tavis Rudd"
 Author = "The Cheetah Development Team"
