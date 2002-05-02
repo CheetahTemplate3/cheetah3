4bec2e6e0b34dcca61f1ce52163bc20aad04f717
tavis_rudd <tavis_rudd>
2002-05-02 23:57:01 +0000

    more on serverSidePath()

--- src/Servlet.py
+++ src/Servlet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Servlet.py,v 1.24 2002/05/02 16:03:54 tavis_rudd Exp $
+# $Id: Servlet.py,v 1.25 2002/05/02 23:57:01 tavis_rudd Exp $
 """Provides an abstract Servlet baseclass for Cheetah's Template class
 
 Meta-Data
@@ -7,12 +7,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@calrudd.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.24 $
+Version: $Revision: 1.25 $
 Start Date: 2001/10/03
-Last Revision Date: $Date: 2002/05/02 16:03:54 $
+Last Revision Date: $Date: 2002/05/02 23:57:01 $
 """ 
 __author__ = "Tavis Rudd <tavis@calrudd.com>"
-__revision__ = "$Revision: 1.24 $"[11:-2]
+__revision__ = "$Revision: 1.25 $"[11:-2]
 
 ##################################################
 ## CONSTANTS & GLOBALS
@@ -52,7 +52,6 @@ except:
 ##################################################
 ## CLASSES
 
-
 class Servlet(CGIImportMixin, BaseServlet):
     
     """This class is an abstract baseclass for Cheetah.Template.Template.
@@ -80,6 +79,12 @@ class Servlet(CGIImportMixin, BaseServlet):
         self.application = transaction.application
         self.response = response = transaction.response
         self.request = transaction.request
+
+        # temporary hack to accomodate bug in WebKit.Servlet.Servlet.serverSidePath
+        # this attribute WILL disappear in the future
+        self._request = transaction.request()
+
+        
         self.session = transaction.session
         self.write = response().write
         #self.writeln = response.writeln
@@ -93,6 +98,7 @@ class Servlet(CGIImportMixin, BaseServlet):
         BaseServlet.sleep(self, transaction)
         self.session = None
         self.request  = None
+        self._request  = None        
         self.response = None
         self.transaction = None
 
