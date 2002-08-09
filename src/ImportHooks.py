f033618a46a0f7d15173f9bf699266e26b531bec
tavis_rudd <tavis_rudd>
2002-08-09 01:43:18 +0000

    *** empty log message ***

--- src/ImportHooks.py
+++ src/ImportHooks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: ImportHooks.py,v 1.9 2002/08/09 01:34:14 tavis_rudd Exp $
+# $Id: ImportHooks.py,v 1.10 2002/08/09 01:43:18 tavis_rudd Exp $
 
 """Provides some import hooks to allow Cheetah's .tmpl files to be imported
 directly like Python .py modules.
@@ -9,12 +9,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@damnsimple.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.9 $
+Version: $Revision: 1.10 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2002/08/09 01:34:14 $
+Last Revision Date: $Date: 2002/08/09 01:43:18 $
 """ 
 __author__ = "Tavis Rudd <tavis@damnsimple.com>"
-__revision__ = "$Revision: 1.9 $"[11:-2]
+__revision__ = "$Revision: 1.10 $"[11:-2]
 
 ##################################################
 ## DEPENDENCIES
@@ -42,6 +42,9 @@ except NameError:
 
 _installed = False
 
+CACHE_PY_FILES = False
+CACHE_DIR = None
+
 ##################################################
 ## CLASSES
 
@@ -53,6 +56,7 @@ class CheetahDirOwner(DirOwner):
 
     def getmod(self, name,
                pathIsDir=os.path.isdir,
+               join=os.path.join,
                newmod=imp.new_module):
         
         tmplPath =  os.path.join(self.path, name + '.tmpl')
@@ -66,7 +70,19 @@ class CheetahDirOwner(DirOwner):
             ## @@ consider adding an ImportError raiser here
             code = str(Compiler(file=tmplPath, moduleName=name,
                                 mainClassName=name))
+            if CACHE_PY_FILES and CACHE_DIR:
+                __file__ = join(CACHE_DIR,
+                                tmplPath.replace('/','_').replace('\\','_'))
+                try:
+                    open(__file__, 'w').write(code)
+                except :
+                    ## @@ TR: need to add some error code here
+                    __file__ = tmplPath
+            else:
+                __file__ = tmplPath
             co = compile(code+'\n', tmplPath, 'exec')
+            
+            
             mod = newmod(name)
             mod.__file__ = co.co_filename
             mod.__co__ = co
