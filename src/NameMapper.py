bdea212318fdb4eaba95e3f9e2597ee2a672f52e
tavis_rudd <tavis_rudd>
2003-03-18 04:41:48 +0000

    revert to version 1.22, without dictionary key changes

--- src/NameMapper.py
+++ src/NameMapper.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: NameMapper.py,v 1.25 2002/11/28 22:20:50 tavis_rudd Exp $
+# $Id: NameMapper.py,v 1.26 2003/03/18 04:41:48 tavis_rudd Exp $
 
 """This module implements Cheetah's optional NameMapper syntax.
 
@@ -63,8 +63,8 @@ Details
 ================================================================================
 The parenthesized letters below correspond to the aims in the second paragraph.
 
-UNIFIED DOTTED NOTATION (a)
----------------------------
+DICTIONARY ACCESS (a)
+---------------------
 
 NameMapper allows access to items in a dictionary using the same dotted notation
 used to access object attributes in Python.  This aspect of NameMapper is known
@@ -81,8 +81,8 @@ This only works with dictionary keys that are also valid python identifiers:
 AUTOCALLING (b,d)
 -----------------
 
-NameMapper automatically detects functions and methods in Cheetah $vars and
-calls them if the parentheses have been left off.  
+NameMapper automatically detects functions and methods in Cheetah $vars and calls
+them if the parentheses have been left off.  
 
 For example if 'a' is an object, 'b' is a method
   $a.b
@@ -98,8 +98,8 @@ Further notes:
 autocalling can only be used with functions or methods that either have no
 arguments or have default values for all arguments.
 
-* NameMapper autocalls only functions and methods.  Classes, instances and
-other callable objects will not be autocalled.
+* NameMapper only autocalls functions and methods.  Classes and callable objects
+will not be autocalled.  
 
 * Autocalling can be disabled using Cheetah's 'useAutocalling' setting.
 
@@ -116,11 +116,8 @@ NAMESPACE CASCADING (d)
 Implementation details
 ================================================================================
 
-* NameMapper's search order is object attributes, then dictionary keys.
-  However, if this is the first "chunk" (identifier) of a searchList lookup
-  -and- the searchList container is a Python dictionary (not a subclass),
-  we ignore the attributes.  That is to avoid calling dict methods like
-  .update() unintentionally.
+* NameMapper's search order is object attributes, then underscored attributes,
+  and finally dictionary items.
 
 * NameMapper.NotFound is raised if a value can't be found for a name.
 
@@ -136,26 +133,22 @@ been compiled, or falls back automatically to the Python version if not.
 Meta-Data
 ================================================================================
 Authors: Tavis Rudd <tavis@damnsimple.com>,
-         Chuck Esterbrook <echuck@mindspring.com>,
-         Mike Orr <iron@mso.oz.net>
-Version: $Revision: 1.25 $
+         Chuck Esterbrook <echuck@mindspring.com>
+Version: $Revision: 1.26 $
 Start Date: 2001/04/03
-Last Revision Date: $Date: 2002/11/28 22:20:50 $
+Last Revision Date: $Date: 2003/03/18 04:41:48 $
 """
 __author__ = "Tavis Rudd <tavis@damnsimple.com>," +\
-             "\nChuck Esterbrook <echuck@mindspring.com>" +\
-         "\nMike Orr <iron@mso.oz.net>"
-__revision__ = "$Revision: 1.25 $"[11:-2]
+             "\nChuck Esterbrook <echuck@mindspring.com>"
+__revision__ = "$Revision: 1.26 $"[11:-2]
 
 ##################################################
 ## DEPENDENCIES
 
 import types
-from types import ClassType, DictType, InstanceType, StringType, TypeType
+from types import StringType, InstanceType, ClassType, TypeType
 import re
-
-# This module requires Python >= 2.0 due to its use of string methods and
-# list comprehensions.
+# it uses the string methods and list comprehensions added in recent versions of python
 
 ##################################################
 ## GLOBALS AND CONSTANTS
@@ -168,106 +161,97 @@ except NameError:
 class NoDefault:
     pass
 
-C_VERSION = False # Has _namemapper.c overridden functions in this module?
-                  # Updated below.
-
 ##################################################
 ## FUNCTIONS
-                  
+
 ## N.B. An attempt is made at the end of this module to import C versions of
 ## these functions.  If _namemapper.c has been compiled succesfully and the
 ## import goes smoothly, the Python versions defined here will be replaced with
 ## the C versions.
+try:
+    from _namemapper import NotFound, valueForKey, valueForName, valueFromSearchList
+    # it is possible, with Jython for example, that _namemapper.c hasn't been compiled
+    C_VERSION = True
+except:
+    C_VERSION = False
 
-class NotFound(LookupError):
-    pass
-class NotFoundInNamespace(NotFound):
-    pass
+    class NotFound(LookupError):
+        pass
+    class NotFoundInNamespace(NotFound):
+        pass
     
-
-def valueForKey(obj, key, searchDictMethods=True):
-    """Get the value of the specified key.  The 'obj' can be a a mapping or
-       any Python object that supports the __getattr__ method. The key can
-       be a mapping item, or an attribute/method.
-       searchDictMethods should be True normally, but False if this is the
-       first "chunk" of a searchList lookup.  It should be True for
-       non-searchList lookups.
-    """
-    searchTheseMethods = searchDictMethods or type(obj) != DictType
-    if hasattr(obj, key) and searchTheseMethods:
-        return getattr(obj, key)
-    try:
-        return obj[key]
-    except (KeyError, AttributeError, TypeError):
-        # KeyError if key is missing
-        # AttributeError if 'obj' is an instance without __getitem__
-        # TypeError if 'obj' is a built-in type without keys
-        raise NotFound, key
+    def valueForKey(obj, key):
+    
+        """Get the value of the specified key.  The 'obj' can be a a mapping or any
+        Python object that supports the __getattr__ method. The key can be a mapping
+        item, or an attribute."""
+    
+        if hasattr(obj, key):
+            return getattr(obj, key)
+        elif hasattr(obj, '__getitem__'):
+            try:
+                return obj[key]
+            except KeyError:
+                raise NotFound, key
+        else:
+            raise NotFound, key
+                
 
 
-def valueForName(obj, name, executeCallables=False):
-    """Get the value for the specified name.  
-       TR says function can be called recursively but MO doesn't believe him.
-    """
+    def valueForName(obj, name, executeCallables=False):
+        """Get the value for the specified name.  This function can be called
+        recursively.  """
+    
+        if type(name)==StringType:
+            # then this is the first call to this function.
+            nameChunks=name.split('.')
+        else:
+            #if this function calls itself then name already is a list of nameChunks
+            nameChunks = name
+            
+        return _valueForName(obj, nameChunks, executeCallables=executeCallables)
+    
+    def _valueForName(obj, nameChunks, executeCallables=False, passNamespace=False):
+        ## go get a binding for the key ##
+        firstKey = nameChunks[0]
+        if passNamespace:
+            try:
+                binding = valueForKey(obj, firstKey)
+            except NotFound:
+                raise NotFoundInNamespace
+        else:
+            binding = valueForKey(obj, firstKey)
+        if executeCallables and callable(binding) and \
+           type(binding) not in (InstanceType, ClassType, TypeType):
+            # the type check allows access to the methods of instances
+            # of classes with __call__() defined
+            # and also allows obj.__class__.__name__
+            binding = binding()
+    
+        if len(nameChunks) > 1:
+            # its a composite name like: nestedObject.item
+            return _valueForName(binding, nameChunks[1:],
+                                executeCallables=executeCallables)
+        else:
+            # its a single key like: nestedObject
+            return binding
 
-    if type(name)==StringType:
-        # then this is the first call to this function.
-        nameChunks=name.split('.')
-    else:
-    # if this function calls itself then name already is a list of
-    # nameChunks
-        nameChunks = name
-        
-    return _valueForName(obj, nameChunks, executeCallables=executeCallables,
-        searchDictMethods=True)
+    def valueFromSearchList(searchList, name, executeCallables=False):
+        if type(name)==StringType:
+            # then this is the first call to this function.
+            nameChunks=name.split('.')
+        else:
+            #if this function calls itself then name already is a list of nameChunks
+            nameChunks = name
 
-
-def _valueForName(obj, nameChunks, executeCallables=False, 
-    passNamespace=False, searchDictMethods=True):
-    ## go get a binding for the key ##
-    firstKey = nameChunks[0]
-    if passNamespace:
-        try:
-            binding = valueForKey(obj, firstKey, searchDictMethods)
-        except NotFound:
-            raise NotFoundInNamespace
-    else:
-        binding = valueForKey(obj, firstKey, searchDictMethods)
-    if executeCallables and callable(binding) and \
-       type(binding) not in (InstanceType, ClassType, TypeType):
-        # the type check allows access to the methods of instances
-        # of classes with __call__() defined
-        # and also allows obj.__class__.__name__
-        binding = binding()
-
-    if len(nameChunks) > 1:
-        # it's a composite name like: nestedObject.item
-        return _valueForName(binding, nameChunks[1:],
-                             executeCallables=executeCallables,
-                             searchDictMethods=True)
-    else:
-        # its a single key like: nestedObject
-        return binding
-
-
-def valueFromSearchList(searchList, name, executeCallables=False):
-    if type(name)==StringType:
-        # then this is the first call to this function.
-        nameChunks=name.split('.')
-    else:
-        # if this function calls itself then name already is a list of
-        # nameChunks
-        nameChunks = name
-
-    for namespace in searchList:
-        try:
-            val = _valueForName(namespace, nameChunks,
-                                executeCallables=executeCallables, 
-                                passNamespace=True, searchDictMethods=False)
-            return val
-        except NotFoundInNamespace:
-            pass           
-    raise NotFound(name)
+        for namespace in searchList:
+            try:
+                val = _valueForName(namespace, nameChunks,
+                                    executeCallables=executeCallables, passNamespace=True)
+                return val
+            except NotFoundInNamespace:
+                pass           
+        raise NotFound(name)
 
 
 ##################################################
@@ -289,13 +273,6 @@ def hasName(obj, name):
         return True
     except NotFound:
         return False
-
-try:
-    from _namemapper import NotFound, valueForKey, valueForName
-    from _namemapper import valueFromSearchList
-    C_VERSION = True
-except ImportError:
-    pass
 
 ##################################################
 ## CLASSES
@@ -355,4 +332,5 @@ def example():
 if __name__ == '__main__':
     example()
 
-# vim: shiftwidth=4 tabstop=4 expandtab
+
+
