ea625aaf00ea7df862b2217dd82470803ee0aa1d
tavis_rudd <tavis_rudd>
2005-01-03 17:58:41 +0000

    stripped out WebwareExp stuff

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: setup.py,v 1.14 2002/03/28 18:14:48 tavis_rudd Exp $
+# $Id: setup.py,v 1.15 2005/01/03 17:58:41 tavis_rudd Exp $
 import os
 
 try:
@@ -7,21 +7,10 @@ try:
 except:
     pass
     
-
-try:                           # see if WebwareExp's Setup procedure can be used
-    from src._properties import Version, Description
-    settings= {'name': 'Cheetah',
-               'version':Version,
-               'description':Description,
-               }
-    from Webware.Setup import SetupManager
-    SetupManager._rebuildManifestTemplate = 0
-    SetupManager(searchPath=['.'],settings=settings)
-except:
-    import SetupTools
-    import SetupConfig
-    configurations = (SetupConfig,)
-    SetupTools.run_setup( configurations )
+import SetupTools
+import SetupConfig
+configurations = (SetupConfig,)
+SetupTools.run_setup( configurations )
 
 
 
