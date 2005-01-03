49ba29ca4a86cb0577665578b5d9ed99392172c9
tavis_rudd <tavis_rudd>
2005-01-03 20:07:53 +0000

    general refactoring

--- src/Tests/Test.py
+++ src/Tests/Test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Test.py,v 1.41 2002/10/20 09:20:20 hierro Exp $
+# $Id: Test.py,v 1.42 2005/01/03 20:07:53 tavis_rudd Exp $
 """Core module of Cheetah's Unit-testing framework
 
 TODO
@@ -14,12 +14,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@damnsimple.com>,
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.41 $
+Version: $Revision: 1.42 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2002/10/20 09:20:20 $
+Last Revision Date: $Date: 2005/01/03 20:07:53 $
 """
 __author__ = "Tavis Rudd <tavis@damnsimple.com>"
-__revision__ = "$Revision: 1.41 $"[11:-2]
+__revision__ = "$Revision: 1.42 $"[11:-2]
 
 
 ##################################################
@@ -45,17 +45,23 @@ import Template
 import FileRefresh
 import CheetahWrapper
 
-testSuite = unittest.findTestCases(SyntaxAndOutput)
-testSuite.addTest( unittest.findTestCases(NameMapper) )
-testSuite.addTest( unittest.findTestCases(Template) )
-testSuite.addTest( unittest.findTestCases(FileRefresh) )
-testSuite.addTest( unittest.findTestCases(CheetahWrapper) )
+SyntaxSuite = unittest.findTestCases(SyntaxAndOutput)
+NameMapperSuite = unittest.findTestCases(NameMapper)
+TemplateSuite = unittest.findTestCases(Template)
+FileRefreshSuite = unittest.findTestCases(FileRefresh)
+CheetahWrapperSuite = unittest.findTestCases(CheetahWrapper)
+
+from SyntaxAndOutput import *
+from NameMapper import *
+from Template import *
+from FileRefresh import *
+from CheetahWrapper import *
 
 ##################################################
 ## if run from the command line
+
 if __name__ == '__main__':
-    unittest.main(testSuite=testSuite)
-    #unittest.TextTestRunner().run(testSuite)
+    unittest.main()
 
 
 
