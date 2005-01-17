7c1270e1579e52d7ec69becb38af9a8c7e83f5c7
tavis_rudd <tavis_rudd>
2005-01-17 19:12:52 +0000

    disabled the unicode patch

--- src/SourceReader.py
+++ src/SourceReader.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: SourceReader.py,v 1.8 2005/01/17 14:54:54 tavis_rudd Exp $
+# $Id: SourceReader.py,v 1.9 2005/01/17 19:12:52 tavis_rudd Exp $
 """SourceReader class for Cheetah's Parser and CodeGenerator
 
 Meta-Data
@@ -7,12 +7,12 @@ Meta-Data
 Author: Tavis Rudd <tavis@damnsimple.com>
 License: This software is released for unlimited distribution under the
          terms of the Python license.
-Version: $Revision: 1.8 $
+Version: $Revision: 1.9 $
 Start Date: 2001/09/19
-Last Revision Date: $Date: 2005/01/17 14:54:54 $
+Last Revision Date: $Date: 2005/01/17 19:12:52 $
 """
 __author__ = "Tavis Rudd <tavis@damnsimple.com>"
-__revision__ = "$Revision: 1.8 $"[11:-2]
+__revision__ = "$Revision: 1.9 $"[11:-2]
 
 import re
 import sys
@@ -34,21 +34,21 @@ class SourceReader:
         ## own. My instinct is to move this up into the code that passes in the
         ## src string rather than leaving it here.  As implemented here it
         ## forces all src strings to unicode, which IMO is not what we want.
-        if encoding is None:
-            # peek at the encoding in the first two lines
-            m = EOLZre.search(src)
-            pos = m.end()
-            if pos<len(src):
-                m = EOLZre.search(src,pos)
-                pos = m.end()
-            m = ENCODINGsearch(src,0,pos)
-            if m:
-                encoding = m.group(1)
-            else:
-                encoding  = sys.getfilesystemencoding()
-        self._encoding = encoding
-        if type(src) is not unicode:
-            src = src.decode(encoding)
+        #  if encoding is None:
+        #      # peek at the encoding in the first two lines
+        #      m = EOLZre.search(src)
+        #      pos = m.end()
+        #      if pos<len(src):
+        #          m = EOLZre.search(src,pos)
+        #          pos = m.end()
+        #      m = ENCODINGsearch(src,0,pos)
+        #      if m:
+        #          encoding = m.group(1)
+        #      else:
+        #          encoding  = sys.getfilesystemencoding()
+        #  self._encoding = encoding
+        #  if type(src) is not unicode:
+        #      src = src.decode(encoding)
         ## end of Terrel's patch
 
         self._src = src
