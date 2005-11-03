0270817bb6bbab5028ec5422d8a8d2bd490564c8
tavis_rudd <tavis_rudd>
2005-11-03 00:18:02 +0000

    fixed treatment of None in several filters

--- src/Filters.py
+++ src/Filters.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: Filters.py,v 1.25 2005/11/03 00:14:06 tavis_rudd Exp $
+# $Id: Filters.py,v 1.26 2005/11/03 00:18:02 tavis_rudd Exp $
 """Filters for the #filter directive; output filters Cheetah's $placeholders .
 
 Filters may now be used standalone, for debugging or for use outside Cheetah.
@@ -10,12 +10,12 @@ would otherwise require a real template object).
 Meta-Data
 ================================================================================
 Author: Tavis Rudd <tavis@damnsimple.com>
-Version: $Revision: 1.25 $
+Version: $Revision: 1.26 $
 Start Date: 2001/08/01
-Last Revision Date: $Date: 2005/11/03 00:14:06 $
+Last Revision Date: $Date: 2005/11/03 00:18:02 $
 """
 __author__ = "Tavis Rudd <tavis@damnsimple.com>"
-__revision__ = "$Revision: 1.25 $"[11:-2]
+__revision__ = "$Revision: 1.26 $"[11:-2]
 
 from StringIO import StringIO # not cStringIO because of unicode support
 
@@ -140,7 +140,7 @@ class MaxLen(Filter):
         
         if val is None:
             return ''
-        output = val
+        output = str(val)
         if kw.has_key('maxlen') and len(output) > kw['maxlen']:
             return output[:kw['maxlen']]
         return output
@@ -169,7 +169,7 @@ class Pager(Filter):
         """Replace None with '' and cut off at maxlen."""
         if val is None:
             return ''
-        output = val
+        output = str(val)
         if kw.has_key('trans') and kw['trans']:
             ID = kw['ID']
             marker = kw.get('marker', '<split>')
@@ -203,7 +203,7 @@ class WebSafe(Filter):
         # Do the default conversion.
         if val is None:
             return ''
-        s = val
+        s = str(val)
         # These substitutions are copied from cgi.escape().
         s = s.replace("&", "&amp;") # Must be done first!
         s = s.replace("<", "&lt;")
@@ -241,7 +241,7 @@ class Strip(Filter):
     def filter(self, val, **kw):
         if val is None:
             return ''
-        s = val
+        s = str(val)
         result = StringIO()
         start = 0   # The current line will be s[start:end].
         while 1: # Loop through each line.
@@ -267,7 +267,7 @@ class StripSqueeze(Filter):
     def filter(self, val, **kw):
         if val is None:
             return ''
-        s = val.split()
+        s = str(val).split()
         return " ".join(s)
     
 ##################################################
