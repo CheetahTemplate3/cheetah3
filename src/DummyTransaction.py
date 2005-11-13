67b089837328804c596fc794b546a4a4074b2eb2
tavis_rudd <tavis_rudd>
2005-11-13 01:12:13 +0000

    switched from StringIO to a simple list for output buffering, after benchmarks showed huge performance benefits

--- src/DummyTransaction.py
+++ src/DummyTransaction.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: DummyTransaction.py,v 1.12 2005/01/03 19:46:48 tavis_rudd Exp $
+# $Id: DummyTransaction.py,v 1.13 2005/11/13 01:12:13 tavis_rudd Exp $
 
 """Provides dummy Transaction and Response classes is used by Cheetah in place
 of real Webware transactions when the Template obj is not used directly as a
@@ -8,14 +8,15 @@ Webware servlet.
 Meta-Data
 ==========
 Author: Tavis Rudd <tavis@damnsimple.com>
-Version: $Revision: 1.12 $
+Version: $Revision: 1.13 $
 Start Date: 2001/08/30
-Last Revision Date: $Date: 2005/01/03 19:46:48 $
+Last Revision Date: $Date: 2005/11/13 01:12:13 $
 """
 __author__ = "Tavis Rudd <tavis@damnsimple.com>"
-__revision__ = "$Revision: 1.12 $"[11:-2]
+__revision__ = "$Revision: 1.13 $"[11:-2]
 
-from StringIO import StringIO  # not cStringIO because of Unicode support, and some strange bug
+def flush():
+    pass
 
 class DummyResponse:
     
@@ -25,18 +26,22 @@ class DummyResponse:
 
     
     def __init__(self):
-        self._stringIO = StringIO()
-        self.write = self._stringIO.write
-        self.flush = self._stringIO.flush
-        self.getvalue = self._stringIO.getvalue
+        self._outputChunks = outputChunks = []
+        self.write = write = outputChunks.append
+        def getvalue(outputChunks=outputChunks):
+            return ''.join(outputChunks)
+        self.getvalue = getvalue
+            
+        def writeln(txt):
+            write(txt)
+            write('\n')
+        self.writeln = writeln        
+        self.flush = flush
 
-        # this might be used for output buffering at some stage in Cheetah
-        # but isn't yet.
-        self.writelines = self._stringIO.writelines
+    def writelines(self, *lines):
+        ## not used
+        [self.writeln(ln) for ln in lines]
         
-    def writeln(self, txt):
-        self.write(txt + '\n')
-
 class DummyTransaction:
 
     """A dummy Transaction class is used by Cheetah in place of real Webware
@@ -44,12 +49,10 @@ class DummyTransaction:
     servlet.
 
     It only provides a response object and method.  All other methods and
-    attributes make no sense in this context.  """
+    attributes make no sense in this context.
+    """
     
-    def __init__(self):
-        self._response = DummyResponse()
-
-    def response(self):
-        """Return a ref to the dummy reponse object."""
-        return self._response
-       
+    def __init__(self, DummyResponse=DummyResponse):       
+        def response(resp=DummyResponse()):
+            return resp
+        self.response = response
