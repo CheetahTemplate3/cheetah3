1fab30524b8fea65e09bb8b0d1d89982ed7d0358
tavis_rudd <tavis_rudd>
2006-01-01 23:52:26 +0000

    more cleanup

--- src/__init__.py
+++ src/__init__.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python
-# $Id: __init__.py,v 1.8 2006/01/01 23:48:18 tavis_rudd Exp $
+# $Id: __init__.py,v 1.9 2006/01/01 23:52:26 tavis_rudd Exp $
 
 """Cheetah is a Python-powered template engine and code-generator.
 It is similar to the Jakarta project's Velocity.
 
 Homepage
 ================================================================================
-http://www.CheetahTemplate.org
+http://www.CheetahTemplate.org/
+
+Documentation
+================================================================================
+For a high-level introduction to Cheetah please refer to the User's Guide
+at http://cheetahtemplate.org/learn.html
 
 Mailing list
 ================================================================================
 cheetahtemplate-discuss@lists.sourceforge.net
-Subscribe at
-http://lists.sourceforge.net/lists/listinfo/cheetahtemplate-discuss
-
-Documentation
-================================================================================
-
-For a high-level introduction to Cheetah please refer to the User's Guide
-at http://cheetahtemplate.org/learn.html
-
-Meta-Data
-================================================================================
-Authors: Tavis Rudd and Mike Orr with contributions from many others.
-License: This software is released for unlimited distribution under the
-         terms of the MIT license. See the LICENSE file.
-Version: $Revision: 1.8 $
-Start Date: 2001/03/30
-Last Revision Date: $Date: 2006/01/01 23:48:18 $
+Subscribe at http://lists.sourceforge.net/lists/listinfo/cheetahtemplate-discuss
 """ 
 __author__ = "Tavis Rudd <tavis@damnsimple.com>"
-__revision__ = "$Revision: 1.8 $"[11:-2]
+__revision__ = "$Revision: 1.9 $"[11:-2]
 
 from Version import Version
