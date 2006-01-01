637303ac6e9f1758c49c2cd89938a194264d2dd6
tavis_rudd <tavis_rudd>
2006-01-01 23:48:18 +0000

    removed out of date install instructions

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: __init__.py,v 1.7 2006/01/01 23:14:54 tavis_rudd Exp $
+# $Id: __init__.py,v 1.8 2006/01/01 23:48:18 tavis_rudd Exp $
 
 """Cheetah is a Python-powered template engine and code-generator.
 It is similar to the Jakarta project's Velocity.
@@ -18,39 +18,18 @@ Documentation
 ================================================================================
 
 For a high-level introduction to Cheetah please refer to the User's Guide
-in the docs/ directory of the Cheetah distribution.
-
-Installation
-================================================================================
-Cheetah can be run a directory in the from the system-wide Python path or
-from a directory in a user's Python path.  
-
-To install Cheetah for a single user:
-  - copy the 'src' sub-directory to a directory called 'Cheetah' that is in the
-  user's PYTHON_PATH
-
-To install Cheetah for system-wide use:
-  - on Posix systems (AIX, Solaris, Linux, IRIX, etc.) become the 'root' user
-  and run: python ./setup.py install
-  
-  - On non-posix systems, such as Windows NT, login as an administrator and
-  type this at the command-line:  python setup.py install
-
-On Posix systems, the system-wide installation will also install the Cheetah's
-command-line compiler program, TScompile, to a system-wide executable path such as
-/usr/local/bin.
- 
+at http://cheetahtemplate.org/learn.html
 
 Meta-Data
 ================================================================================
 Authors: Tavis Rudd and Mike Orr with contributions from many others.
 License: This software is released for unlimited distribution under the
          terms of the MIT license. See the LICENSE file.
-Version: $Revision: 1.7 $
+Version: $Revision: 1.8 $
 Start Date: 2001/03/30
-Last Revision Date: $Date: 2006/01/01 23:14:54 $
+Last Revision Date: $Date: 2006/01/01 23:48:18 $
 """ 
 __author__ = "Tavis Rudd <tavis@damnsimple.com>"
-__revision__ = "$Revision: 1.7 $"[11:-2]
+__revision__ = "$Revision: 1.8 $"[11:-2]
 
 from Version import Version
