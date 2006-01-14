f74df141b329317a3c7e1a02da2b952a061a603a
tavis_rudd <tavis_rudd>
2006-01-14 04:44:07 +0000

    updated

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-# $Id: __init__.py,v 1.9 2006/01/01 23:52:26 tavis_rudd Exp $
+# $Id: __init__.py,v 1.10 2006/01/14 04:44:07 tavis_rudd Exp $
 
-"""Cheetah is a Python-powered template engine and code-generator.
-It is similar to the Jakarta project's Velocity.
+"""Cheetah is an open source template engine and code generation tool.
+
+It can be used standalone or combined with other tools and frameworks. Web
+development is its principle use, but Cheetah is very flexible and is also being
+used to generate C++ game code, Java, sql, form emails and even Python code.
 
 Homepage
 ================================================================================
@@ -19,6 +22,6 @@ cheetahtemplate-discuss@lists.sourceforge.net
 Subscribe at http://lists.sourceforge.net/lists/listinfo/cheetahtemplate-discuss
 """ 
 __author__ = "Tavis Rudd <tavis@damnsimple.com>"
-__revision__ = "$Revision: 1.9 $"[11:-2]
+__revision__ = "$Revision: 1.10 $"[11:-2]
 
 from Version import Version
