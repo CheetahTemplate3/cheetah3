a7c4db376de382bb85a0101e4cd91840d71968bb
tavis_rudd <tavis_rudd>
2006-01-15 01:49:23 +0000

    updated descriptions

--- SetupConfig.py
+++ SetupConfig.py
@@ -2,7 +2,7 @@
 name = "Cheetah"
 from src.Version import Version as version
 maintainer = "Tavis Rudd"
-author = "The Cheetah Development Team"
+author = "Tavis Rudd"
 author_email = "cheetahtemplate-discuss@lists.sf.net"
 url = "http://www.CheetahTemplate.org/"
 packages = ['Cheetah',
@@ -52,11 +52,68 @@ scripts = ['bin/cheetah-compile',
 data_files = ['recursive: src *.tmpl *.txt LICENSE README TODO CHANGES',
               ]
 
-## GET THE DESCRIPTION AND CREATE THE README
-from src import __doc__  
-README = open('README','w')
-README.write(__doc__)
-README.close()
+description = "Cheetah is a template engine and code generation tool."
 
-description = __doc__.split('\n')[0]
-long_description = __doc__
+long_description = '''Cheetah is an open source template engine and code generation tool.
+
+It can be used standalone or combined with other tools and frameworks. Web
+development is its principle use, but Cheetah is very flexible and is also being
+used to generate C++ game code, Java, sql, form emails and even Python code.
+
+Documentation
+================================================================================
+For a high-level introduction to Cheetah please refer to the User\'s Guide
+at http://cheetahtemplate.org/learn.html
+
+Mailing list
+================================================================================
+cheetahtemplate-discuss@lists.sourceforge.net
+Subscribe at http://lists.sourceforge.net/lists/listinfo/cheetahtemplate-discuss
+
+Credits
+================================================================================
+http://cheetahtemplate.org/credits.html
+
+Praise
+================================================================================
+"I\'m enamored with Cheetah" - Sam Ruby, senior member of IBM Emerging
+Technologies Group & director of Apache Software Foundation
+
+"Give Cheetah a try. You won\'t regret it. ... Cheetah is a truly powerful
+system. ... Cheetah is a serious contender for the 'best of breed' Python
+templating." - Alex Martelli
+
+"People with a strong PHP background absolutely love Cheetah for being Smarty,
+but much, much better." - Marek Baczynski
+
+"I am using Smarty and I know it very well, but compiled Cheetah Templates with
+its inheritance approach is much powerful and easier to use than Smarty." -
+Jaroslaw Zabiello
+
+"There is no better solution than Cheetah" - Wilk
+
+"A cheetah template can inherit from a python class, or a cheetah template, and
+a Python class can inherit from a cheetah template. This brings the full power
+of OO programming facilities to the templating system, and simply blows away
+other templating systems" - Mike Meyer
+
+"Cheetah has successfully been introduced as a replacement for the overweight
+XSL Templates for code generation. Despite the power of XSL (and notably XPath
+expressions), code generation is better suited to Cheetah as templates are much
+easier to implement and manage." - The FEAR development team
+    (http://fear.sourceforge.net/docs/latest/guide/Build.html#id2550573)
+
+"I\'ve used Cheetah quite a bit and it\'s a very good package" - Kevin Dangoor,
+lead developer of TurboGears.
+
+Recent Changes
+================================================================================
+See http://cheetahtemplate.org/docs/CHANGES for full details.
+
+'''
+try:
+    recentChanges = open('CHANGES').read().split('\n1.0')[0]
+    long_description += recentChanges
+    del recentChanges
+except:
+    pass
