2b3ff07cb191aa688e1ba54c2d3c931c4cd1f831
tavis_rudd <tavis_rudd>
2007-04-04 01:13:22 +0000

    bump version

--- src/Version.py
+++ src/Version.py
@@ -1,5 +1,5 @@
-Version = '2.0rc7'
-VersionTuple = (2,0,0,'candidate',7)
+Version = '2.0rc8'
+VersionTuple = (2,0,0,'candidate',8)
 
 MinCompatibleVersion = '2.0rc6'
 MinCompatibleVersionTuple = (2,0,0,'candidate',6)
