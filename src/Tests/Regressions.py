9643c12a7d835ee83cf0052e14172d3818fb950e
R. Tyler Ballance <tyler@monkeypox.org>
2009-01-31 15:11:19 -0800

    Add the Regressions.py file for regression tests for tracking down bugs
    The first issue I'm adding into here is an error where NameMapper will raise a Cheetah.NameMapper.NotFound exception when accessing an attribute that raises an exception (example class included)
    
    Signed-off-by: R. Tyler Ballance <tyler@monkeypox.org>

---
+++ src/Tests/Regressions.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import Cheetah.NameMapper 
+import Cheetah.Template
+
+import unittest
+
+class GetAttrException(Exception):
+    pass
+
+class CustomGetAttrClass(object):
+    def __getattr__(self, name):
+        raise GetAttrException('FAIL, %s' % name)
+
+class GetAttrTest(unittest.TestCase):
+    '''
+        Test for an issue occurring when __getatttr__() raises an exception
+        causing NameMapper to raise a NotFound exception
+    '''
+    def test_ValidException(self):
+        o = CustomGetAttrClass()
+        try:
+            print o.attr
+        except GetAttrException, e:
+            # expected
+            return
+        except:
+            self.fail('Invalid exception raised: %s' % e)
+        self.fail('Should have had an exception raised')
+
+    def test_NotFoundException(self):
+        template = '''
+            #def raiseme()
+                $obj.attr
+            #end def'''
+
+        template = Cheetah.Template.Template.compile(template, compilerSettings={}, keepRefToGeneratedCode=True)
+        template = template(searchList=[{'obj' : CustomGetAttrClass()}])
+        assert template, 'We should have a vallid template object by now'
+
+        self.failUnlessRaises(GetAttrException, template.raiseme)
+
+if __name__ == '__main__':
+    unittest.main()
