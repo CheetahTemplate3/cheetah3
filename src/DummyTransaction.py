b42efa06e59a6534d0d8db0eb2b5f796fca91871
R. Tyler Ballance <tyler@slide.com>
2009-05-13 16:44:18 -0700

    Tidy up DummyTransaction as it was getting on my nerves a bit
    Signed-off-by: R. Tyler Ballance <tyler@slide.com>

--- src/DummyTransaction.py
+++ src/DummyTransaction.py
@@ -1,32 +1,22 @@
 #!/usr/bin/env python
-# $Id: DummyTransaction.py,v 1.13 2005/11/13 01:12:13 tavis_rudd Exp $
 
-"""Provides dummy Transaction and Response classes is used by Cheetah in place
+'''
+Provides dummy Transaction and Response classes is used by Cheetah in place
 of real Webware transactions when the Template obj is not used directly as a
 Webware servlet.
-
-Meta-Data
-==========
-Author: Tavis Rudd <tavis@damnsimple.com>
-Version: $Revision: 1.13 $
-Start Date: 2001/08/30
-Last Revision Date: $Date: 2005/11/13 01:12:13 $
-"""
-__author__ = "Tavis Rudd <tavis@damnsimple.com>"
-__revision__ = "$Revision: 1.13 $"[11:-2]
+'''
 
 import types
 
 def flush():
     pass
 
-class DummyResponse:
-    
-    """A dummy Response class is used by Cheetah in place of real Webware
-    Response objects when the Template obj is not used directly as a Webware
-    servlet.  """
-
-    
+class DummyResponse(object):
+    '''
+        A dummy Response class is used by Cheetah in place of real Webware
+        Response objects when the Template obj is not used directly as a Webware
+        servlet
+    ''' 
     def __init__(self):
         self._outputChunks = outputChunks = []
         self.write = write = outputChunks.append
@@ -44,20 +34,22 @@ class DummyResponse:
         ## not used
         [self.writeln(ln) for ln in lines]
         
-class DummyTransaction:
+class DummyTransaction(object):
+    '''
+        A dummy Transaction class is used by Cheetah in place of real Webware
+        transactions when the Template obj is not used directly as a Webware
+        servlet.
 
-    """A dummy Transaction class is used by Cheetah in place of real Webware
-    transactions when the Template obj is not used directly as a Webware
-    servlet.
+        It only provides a response object and method.  All other methods and
+        attributes make no sense in this context.
+    '''
+    def __init__(self, *args, **kwargs):
+        self._response = None
 
-    It only provides a response object and method.  All other methods and
-    attributes make no sense in this context.
-    """
-    
-    def __init__(self, DummyResponse=DummyResponse):       
-        def response(resp=DummyResponse()):
-            return resp
-        self.response = response
+    def response(self, resp=None):
+        if self._response is None:
+            self._response = resp or DummyResponse()
+        return self._response
 
 class TransformerResponse(object):
     def __init__(self, *args, **kwargs):
