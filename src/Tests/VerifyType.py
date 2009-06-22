c8cd94159ba47afce395b55643410f92352aeb55
R. Tyler Ballance <tyler@slide.com>
2009-06-21 17:01:48 -0700

    Add a test to verify the functionality of verifyTypeClass()

--- src/Tests/VerifyType.py
+++ src/Tests/VerifyType.py
@@ -27,6 +27,27 @@ class VerifyType_Test(unittest.TestCase):
         self.failUnlessRaises(TypeError, _verifytype.verifyType, arg,
                     'arg', legalTypes, 'list or dict')
 
+class FakeClass(dict):
+    pass
+
+class VerifyTypeClass_Test(unittest.TestCase):
+    def test_Verified(self):
+        arg = FakeClass
+        legalTypes = [type]
+        try:
+            rc = VerifyType.VerifyTypeClass(arg, 'arg', legalTypes, '', dict) 
+            assert rc
+        except TypeError:
+            self.fail('Should not have raised a TypeError here')
+
+        try:
+            rc = _verifytype.verifyTypeClass(arg, 'arg', legalTypes, 'foo', dict)
+            assert rc
+        except TypeError:
+            self.fail('Should not have raised a TypeError here')
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
