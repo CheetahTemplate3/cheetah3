3d67e89fb967a393c47ce2f0b199a6e1f554b790
R. Tyler Ballance <tyler@monkeypox.org>
2009-10-15 11:48:56 -0700

    Revert "Bump the version to Cheetah 2.4.0"
    This reverts commit 8237cf474f5398ec737c1f9bc511cbb40b7655bb.

--- cheetah/Version.py
+++ cheetah/Version.py
@@ -1,5 +1,5 @@
-Version = '2.4.0'
-VersionTuple = (2, 4, 0, 'final', 0)
+Version = '2.3.0'
+VersionTuple = (2, 3, 0, 'development', 0)
 
 MinCompatibleVersion = '2.0rc6'
 MinCompatibleVersionTuple = (2,0,0,'candidate',6)
