CheetahTemplate3/cheetah3

commit a0c6979f8f3882f3994e25be4648e9a70c8876e0

Prune unnecessary 'types' import in Namemapper.py

cheetah/NameMapper.py
@@ -1,5 +1,4 @@
-# $Id: NameMapper.py,v 1.32 2007/12/10 19:20:09 tavis_rudd Exp $
-
+#!/usr/bin/env python
 """This module supports Cheetah's optional NameMapper syntax.
 
 Overview
@@ -142,8 +141,6 @@ Last Revision Date: $Date: 2007/12/10 19:20:09 $
 __author__ = "Tavis Rudd <tavis@damnsimple.com>," +\
              "\nChuck Esterbrook <echuck@mindspring.com>"
 __revision__ = "$Revision: 1.32 $"[11:-2]
-import types
-from types import StringType, InstanceType, ClassType, TypeType
 from pprint import pformat
 import inspect
 import pdb

commit 62f5a2fe995695c1a88cef5e059192b20143afe0

Fix regression in the pure Python NameMapper
Introduced the regression in `a0c6979` when cleaning up
imports.

cheetah/NameMapper.py
@@ -179,7 +179,7 @@ def _wrapNotFoundException(exc, fullName, namespace):
         raise
 
 def _isInstanceOrClass(obj):
-    if type(obj) in (InstanceType, ClassType):
+    if isinstance(obj, type):
         # oldstyle
         return True
 