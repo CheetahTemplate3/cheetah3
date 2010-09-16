f8e2a8ea51a280cdd1f6309ce95662c956f2d8d0
R. Tyler Croy <tyler@monkeypox.org>
2010-09-16 13:04:52 -0700

    Bump the version for the next series of development
    Change-Id: I04a6df632c2b0c95128ef78a766514f2da5e756a

--- cheetah/Version.py
+++ cheetah/Version.py
@@ -1,5 +1,5 @@
-Version = '2.4.3'
-VersionTuple = (2, 4, 3, 'development', 0)
+Version = '2.4.4'
+VersionTuple = (2, 4, 4, 'development', 0)
 
 MinCompatibleVersion = '2.0rc6'
 MinCompatibleVersionTuple = (2, 0, 0, 'candidate', 6)
