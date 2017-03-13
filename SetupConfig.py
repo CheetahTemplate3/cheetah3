a4ae35de4c194a755d4329a705d85ac4afafce2a
Oleg Broytman <phd@phdru.name>
2017-03-13 05:36:47 +0300

    Fix trove classifiers: remove excessive commas

--- SetupConfig.py
+++ SetupConfig.py
@@ -26,8 +26,8 @@ classifiers = [line.strip() for line in '''\
   License :: OSI Approved :: MIT License
   Operating System :: OS Independent
   Programming Language :: Python
-  Programming Language :: Python :: 2,
-  Programming Language :: Python :: 2.7,
+  Programming Language :: Python :: 2
+  Programming Language :: Python :: 2.7
   Topic :: Internet :: WWW/HTTP
   Topic :: Internet :: WWW/HTTP :: Dynamic Content
   Topic :: Internet :: WWW/HTTP :: Site Management
