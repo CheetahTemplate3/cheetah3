601c68b9dfdb9515f10c8fad555e3b9e4b6e8b0f
Oleg Broytman <phd@phdru.name>
2018-06-21 20:08:05 +0300

    Feat(LoadTemplate): Improve loadTemplate
    Cleanup directory. Add all path components to `sys.modules`.

--- Cheetah/LoadTemplate.py
+++ Cheetah/LoadTemplate.py
@@ -1,3 +1,4 @@
+import imp
 import os
 import sys
 from Cheetah.ImportHooks import CheetahDirOwner
@@ -13,14 +14,31 @@ def loadTemplate(templatePath, debuglevel=0):
     *.py are byte-compiled to *.py[co]. Compiled files are cached
     in the template directory. Errors on writing are silently ignored.
     """
-    dirname, filename = os.path.split(templatePath)
+    drive, localPath = os.path.splitdrive(templatePath)
+    dirname, filename = os.path.split(localPath)
     filename, ext = os.path.splitext(filename)
-    template_dir = CheetahDirOwner(dirname)
+    if dirname:
+        # Cleanup: Convert /Templates//views/ -> /Templates/views
+        dirname_list = dirname.replace(os.sep, '/').split('/')
+        dirname_list = [d for (i, d) in enumerate(dirname_list)
+                        if i == 0 or d]  # Preserve root slash
+        dirname = os.sep.join(dirname_list)
+        # Add all "modules" to sys.modules
+        components = []
+        for d in dirname_list:
+            components.append(d)
+            _mod_name = '.'.join(components)
+            _mod = imp.new_module(_mod_name)
+            _d = os.path.abspath(os.sep.join(components))
+            _mod.__file__ = _d
+            _mod.__path__ = [_d]
+            sys.modules[_mod_name] = _mod
+    template_dir = CheetahDirOwner(drive + dirname)
     if ext:
         template_dir.templateFileExtensions = (ext,)
     template_dir.debuglevel = debuglevel
     mod = template_dir.getmod(filename)
-    fqname = templatePath.replace(os.sep, '.')
+    fqname = os.path.join(dirname, filename).replace(os.sep, '.')
     mod.__name__ = fqname
     sys.modules[fqname] = mod
     co = mod.__co__
