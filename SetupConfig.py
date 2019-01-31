4ade1ca2eed2ab8dea5ea71332a6b8a82bb0dd25
Oleg Broytman <phd@phdru.name>
2019-02-01 02:16:47 +0300

    Python 3.7

--- SetupConfig.py
+++ SetupConfig.py
@@ -54,6 +54,7 @@ classifiers = [line.strip() for line in '''\
   Programming Language :: Python :: 3.4
   Programming Language :: Python :: 3.5
   Programming Language :: Python :: 3.6
+  Programming Language :: Python :: 3.7
   Topic :: Internet :: WWW/HTTP
   Topic :: Internet :: WWW/HTTP :: Dynamic Content
   Topic :: Internet :: WWW/HTTP :: Site Management
