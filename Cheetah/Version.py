e67cb88ae8c9f2f80ebf07176cdc7cc2cbb83ae9
Oleg Broytman <phd@phdru.name>
2020-05-16 13:45:34 +0300

    Release 3.2.5b6

--- Cheetah/Version.py
+++ Cheetah/Version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-Version = '3.2.5b5'
-VersionTuple = (3, 2, 5, 'beta', 5)
+Version = '3.2.5b6'
+VersionTuple = (3, 2, 5, 'beta', 6)
 
 MinCompatibleVersion = '3.0.0a1'
 MinCompatibleVersionTuple = (3, 0, 0, 'alpha', 1)
